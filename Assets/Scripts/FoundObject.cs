3ef1e89e275c9081dd34ff851ea4e35819409ba6
swhalen2128 <sew117@georgetown.edu>
2018-04-24 17:25:08 -0400

    Unity files

---
+++ Assets/Scripts/FoundObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine.Video;
+
+
+
+public class FoundObject : MonoBehaviour, IFocusable {
+    public GameObject prize;
+    private Vector3 loc;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnFocusEnter()
+    {
+
+        loc = gameObject.transform.position;
+        
+        Instantiate(prize);
+        prize.transform.position = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        Destroy(gameObject);
+
+    }
+
+    public void OnFocusExit()
+    {
+        
+    }
+
+
+}
---
+++ Assets/Scripts/GazeHandler.cs
@@ -0,0 +1,36 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GazeHandler : MonoBehaviour, IFocusable {
+
+    private Material mat;
+    public Material vid;
+    public GameObject vdi1;
+    
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void OnFocusEnter()
+    {
+        var com = gameObject.GetComponent<Renderer>();
+        mat = com.material;
+        com.material = vid;
+        vdi1.GetComponent<VideoPlayer>().Play();
+    }
+
+    public void OnFocusExit()
+    {
+        vdi1.GetComponent<VideoPlayer>().Pause();
+    }
+}
